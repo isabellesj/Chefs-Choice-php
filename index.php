958dc4ff51f5d06a0a288efc880c0b6861fd6a3f
Isabelle Schröder Jockovic <isabelle.sj@live.se>
2024-04-10 12:10:10 +0200

    fixed basic styling for navigation

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once ("Models/Product.php");
 require_once ("Models/Database.php");
 require_once ("Utils/UrlModifier.php");
+require_once ("Pages/layout/navigation.php");
 
 
 $sortOrder = $_GET['sortOrder'] ?? "";
@@ -25,53 +26,17 @@ $urlModifier = new UrlModifier();
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Shop Homepage - Start Bootstrap Template</title>
-    <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="/css/styles.css" rel="stylesheet" />
+    <link href="/css/style.css" rel="stylesheet" />
 </head>
 
 <body>
     <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="#!">Chef's Choice</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">Categorier</a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#!">All Products</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <?php
-                            foreach ($dbContext->getAllCategories() as $category) {
-                                echo "<li><a class='dropdown-item' href='#!'>$category->title</a></li> ";
-                            }
-                            ?>
-
-                        </ul>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href="#!">Login</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">Create account</a></li>
-                </ul>
-                <form class="d-flex">
-                    <button class="btn btn-outline-dark" type="submit">
-                        <i class="bi-cart-fill me-1"></i>
-                        Cart
-                        <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                    </button>
-                </form>
-            </div>
-        </div>
-    </nav>
+    <?php
+    layout_navigation($dbContext)
+        ?>
     <!-- Header-->
     <header class="bg-dark py-5">
         <div class="container px-4 px-lg-5 my-5">
@@ -79,7 +44,7 @@ $urlModifier = new UrlModifier();
                 <?php
                 // $hour = date('h');
                 // if ($hour >= 9) {
-                //             ?>
+                //                     ?>
                 // <h1 class="display-4 fw-bolder">Super shoppen</h1>
                 //
                 <?php
